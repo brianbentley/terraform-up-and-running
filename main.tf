d4d1623ff572592354eb90630de1fb1af8f397cb
Brian Bentley <bbentley@mailworks.org>
2022-11-10 17:33:56 -0500

    Added user_data script

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ resource "aws_instance" "example" {
   ami           = "ami-0d5bf08bc8017c83b"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
   tags = {
     "Name" = "terraform-example"
   }
